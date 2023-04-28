50b5d4c7d3c13c89f1d400bc90c4437c9372192d
santosh <kumarpradhan.santosh@gmail.com>
2023-04-28 08:35:41 +0530

    2nd commit

--- src/test/java/demoPack/MyPrintClass.java
+++ src/test/java/demoPack/MyPrintClass.java
@@ -8,5 +8,6 @@ public static void main(String[] args)
 	
 	System.out.println("This is a demo print statement");
 	System.out.println("This is a first commit statement");
+	System.out.println("This is a second commit statement");
 	}
 }
